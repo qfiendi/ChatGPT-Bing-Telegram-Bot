c1d10bd1f523de30a01ad3b1764ba1f77a3d912e
sefn <140893701+qfiendi@users.noreply.github.com>
2023-07-29 13:28:14 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 WEB_HOOK = os.environ["WEB_HOOK"]
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.environ["MTEzNDgwMzY5MzQ1NjM0NzEzNg.GmopX-.1h57nTzVbZoZxMRsoktBQoNyHcq0VBBhpKMoKo"]
 PORT = int(os.environ.get('PORT', '8080'))
 NICK = os.environ.get('NICK', None)
 API = os.environ.get('API', None)
